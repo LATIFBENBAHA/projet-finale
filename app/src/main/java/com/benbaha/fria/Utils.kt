220ca83714a0635c56cbef1e0fecc762649a7729
LATIFBENBAHA <abdellatif.benbaha@etu.univ-amu.fr>
2024-06-12 09:35:16 +0200

    comit semi finale

---
+++ app/src/main/java/com/benbaha/fria/Utils.kt
@@ -0,0 +1,8 @@
+package com.benbaha.fria
+
+class Utils {
+    companion object {
+        const val TOKEN = "my_token_value"
+        const val EXPIRY_TIME = 24 * 60 * 60 * 1000 // 24 hours in milliseconds
+    }
+}
